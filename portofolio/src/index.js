d856caf1a848a464bf675b061b245ef4c4c5c122
stoicamirela <stoicamirela97@gmail.com>
2023-10-09 00:20:02 +0300

    imported the brands iccons in index.js

--- portofolio/src/index.js
+++ portofolio/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 import App from './App';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
+library.add(fab);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
