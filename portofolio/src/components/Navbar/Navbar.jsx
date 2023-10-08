a24907d9d07ef0b89b10e959e6476e8e169baa31
stoicamirela <stoicamirela97@gmail.com>
2023-10-08 17:47:42 +0300

    functional navbar, needs to collapse when scrolling

--- portofolio/src/components/Navbar/Navbar.jsx
+++ portofolio/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./Navbar.css";
 import logo from "../../assets/images/GD.png";
 
+const NavbarComponent = () => {
+  const [isSticky, setIsSticky] = useState(false);
 
-export default function NavbarComponent() {
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsSticky(window.scrollY > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div className="container">
-      <Navbar expand="lg" className="bg-body-tertiary navbar justify-content-between">
+      <Navbar expand="lg" className={`bg-body-tertiary navbar justify-content-between ${isSticky ? 'sticky' : ''}`}>
         <Nav>
           <img src={process.env.PUBLIC_URL + logo} className="App-logo" alt="logo" />  
-          <Nav.Link className="nav-link" href="#home">Home</Nav.Link>
           <Nav.Link className="nav-link" href="#about">About</Nav.Link>
           <Nav.Link className="nav-link" href="#services">Our Services</Nav.Link>
           <Nav.Link className="nav-link" href="#contact">Contact</Nav.Link>
@@ -18,4 +31,6 @@ export default function NavbarComponent() {
       </Navbar>
     </div>
   );
-}
+};
+
+export default NavbarComponent;
--- portofolio/src/components/Page_Content/Page_Content.jsx
+++ portofolio/src/components/Page_Content/Page_Content.jsx
@@ -6,15 +6,65 @@ export default function PageComponent() {
       <h1>About us</h1>
       <div className={s.page_content} id="about">
         <p>
-        Hello there! At GraphicDesign.io, we offer a range of specialized services to help businesses achieve their design goals. Here are some of the services we provide:
+          At GraphicDesign.io, we were founded in 2015 by a team of passionate
+          designers who wanted to help businesses achieve their design goals.
+          Our team has grown to include over 50 employees who are dedicated to
+          providing high-quality design services to clients. We believe that
+          effective communication is key to successful design, and we work
+          closely with our clients to ensure that their design needs are met.
+          Our goal is to provide innovative and effective design solutions that
+          help businesses stand out in their respective industries. We take
+          pride in our work and strive to provide our clients with high-quality
+          designs that effectively represent their brand image.
+        </p>
+      </div>
+      <h1>Our services</h1>
+      <div className={s.page_content} id="services">
+        <p>
+          We at GraphicDesign.io, we offer a range of specialized services to
+          help businesses achieve their design goals. Here are some of the
+          services we provide:
+        </p>
         <ul>
-            <li><b>Logo Design:</b> We can help you create a unique and visually appealing logo that effectively represents your brand image.</li>
-            <li><b>Web Design:</b> Our team of professional designers can help you design a website that is visually appealing, user-friendly, and effectively represents your brand image.</li>
-            <li><b>Brochure Design:</b> We can help you design brochures that are visually appealing and informative, providing your customers with all the information they need about your products or services.</li>
-            <li><b>Marketing Collateral Design:</b> We can help you design marketing collateral, such as flyers, posters, and social media graphics, that are visually appealing and effective in promoting your business.</li>
-            <li><b>Illustration Design:</b> We can help you design illustrations that are unique and visually appealing, adding a touch of creativity to your marketing materials.</li>
-            <li><b>Product Design:</b> We can help you design product pages that showcase your products, services, or ideas, as well as product packaging that is visually appealing and effective in promoting your products.</li>
+          <li>
+            <b>Logo Design:</b> We can help you create a unique and visually
+            appealing logo that effectively represents your brand image.
+          </li>
+          <li>
+            <b>Web Design:</b> Our team of professional designers can help you
+            design a website that is visually appealing, user-friendly, and
+            effectively represents your brand image.
+          </li>
+          <li>
+            <b>Brochure Design:</b> We can help you design brochures that are
+            visually appealing and informative, providing your customers with
+            all the information they need about your products or services.
+          </li>
+          <li>
+            <b>Marketing Collateral Design:</b> We can help you design marketing
+            collateral, such as flyers, posters, and social media graphics, that
+            are visually appealing and effective in promoting your business.
+          </li>
+          <li>
+            <b>Illustration Design:</b> We can help you design illustrations
+            that are unique and visually appealing, adding a touch of creativity
+            to your marketing materials.
+          </li>
+          <li>
+            <b>Product Design:</b> We can help you design product pages that
+            showcase your products, services, or ideas, as well as product
+            packaging that is visually appealing and effective in promoting your
+            products.
+          </li>
         </ul>
+        {/* </p> */}
+      </div>
+      <h1>Contact</h1>
+      <div className={s.page_content} id="contact">
+        <p>
+          For any request or questions do not hesitate t contact us at the
+          following email address:
+          <ul></ul>
         </p>
       </div>
     </div>
