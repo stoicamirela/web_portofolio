b4be7bebde48c3847b3f6da99924a470eb972620
stoicamirela <stoicamirela97@gmail.com>
2023-10-08 19:19:33 +0300

    change the id directly to the headers, removed sticky since it won't be visible on seamless scrolling effect

--- portofolio/src/components/Navbar/Navbar.jsx
+++ portofolio/src/components/Navbar/Navbar.jsx
@@ -1,27 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./Navbar.css";
 import logo from "../../assets/images/GD.png";
 
 const NavbarComponent = () => {
-  const [isSticky, setIsSticky] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsSticky(window.scrollY > 0);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
   return (
     <div className="container">
-      <Navbar expand="lg" className={`bg-body-tertiary navbar justify-content-between ${isSticky ? 'sticky' : ''}`}>
+      <Navbar expand="lg" className={`bg-body-tertiary navbar justify-content-between`}>
         <Nav>
           <img src={process.env.PUBLIC_URL + logo} className="App-logo" alt="logo" />  
           <Nav.Link className="nav-link" href="#about">About</Nav.Link>
